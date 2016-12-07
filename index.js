334e26d1f45c52e161677738844a6523f6393c2d
Michiel Kalkman <michiel@nosuchtype.com>
2016-12-07 09:58:17 +0100

    Read config from config/ember-intl

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 /* jshint node: true */
 'use strict';
 
-const fs     = require('fs');
-const glob   = require('glob');
-const mkdirp = require('mkdirp');
-const YAML   = require('yamljs');
+const fs      = require('fs');
+const process = require('process');
+const glob    = require('glob');
+const mkdirp  = require('mkdirp');
+const YAML    = require('yamljs');
 
 const POTGenerator  = require('./lib/pot-generator');
 const YAMLGenerator = require('./lib/yaml-generator');
@@ -12,14 +13,28 @@ const YAMLGenerator = require('./lib/yaml-generator');
 const pofileLocaleRegExp = /\/(\w*-\w*)\.po/;
 
 const i18nDirs = [
-  'translations',
   'i18n-data',
   'i18n-data/po',
   'i18n-data/pot'
   ];
 
+
+const getConfig = ()=> {
+  const cwd = process.cwd();
+  const config = require(`${cwd}/config/ember-intl`)();
+  return {
+    baseLocale : config['baseLocale'],
+    locales    : config['locales'],
+    targetDir  : config['inputPath']
+  };
+}
+
+const getI18nDirs = ()=> {
+  return i18nDirs.concat(getConfig().targetDir);
+}
+
 const dirsExist = ()=> {
-  return i18nDirs.every(fs.existsSync);
+  return getI18nDirs().every(fs.existsSync);
 }
 
 const collectErrors = ()=> {
@@ -60,6 +75,7 @@ module.exports = {
             return;
           }
           const files = glob.sync("i18n-data/po/*.po");
+          const config = getConfig();
           files.forEach(
             (fileName)=> {
               const locale = fileName.match(pofileLocaleRegExp)[1];
@@ -71,7 +87,7 @@ module.exports = {
               const yamlGenerator = new YAMLGenerator(pofile);
               yamlGenerator.run();
 
-              const outPath = `translations/${locale}.yaml`;
+              const outPath = `${config.targetDir}/${locale}.yaml`;
 
               console.log(` - Writing ${locale} to ${outPath}`);
               fs.writeFileSync(
@@ -90,7 +106,8 @@ module.exports = {
           if (!allOK()) {
             return;
           }
-          const yamlData = YAML.load('translations/en-us.yaml');
+          const config = getConfig();
+          const yamlData = YAML.load(`${config.targetDir}/${config.baseLocale}.yaml`);
           const potGenerator = new POTGenerator(yamlData);
           potGenerator.run();
 
@@ -108,7 +125,9 @@ module.exports = {
         description: 'Generate the translations, i18n-data dirs if they do not exist',
         run: function(commandOptions, rawArgs) {
           console.log('Generating i18n-data dirs');
-          i18nDirs.forEach((dir)=> {
+          getConfig();
+
+          getI18nDirs().forEach((dir)=> {
             if (fs.existsSync(dir)) {
               console.log(` - ${dir} exists`);
             } else {
